7f909addcee38505ecd6bb32c4568cb207e6a445
AK1715 <ns031077@gmail.com>
2026-02-10 15:09:12 +0530

    Singly Linked List

--- Assign1.py
+++ Assign1.py
@@ -21,13 +21,44 @@ class SLL:
                 temp=temp.next
             temp.next=n
         else:
-            temp.next=n
+            self.start=n
     def search(self,data):
         temp=self.start
         while temp is not None:
             if temp.item == data:
-                return data
+                return temp
             temp=temp.next
+    def insert_after(self,findValue,data):
+        temp=self.start
+        n=Node(data)
+        while temp is not None:
+            if temp.item == findValue:
+                    n.next=temp.next
+                    temp.next=n
+            temp=temp.next
+    def delete_start(self):
+        if self.start is not None:
+            self.start=self.start.next
+        else:
+            print("List is empty")
+    def delete_last(self):
+        # if self.start == None:
+        #     return print("List is Empty")
+        # if self.start.next == None:
+        #     self.start=None
+        #     return
+        # temp=self.start
+        # next=temp.next
+        # while next.next is not None:
+        #     temp=temp.next
+        #     next=next.next
+        # temp.next = None
+        if self.start == None:
+            return print("List is empty")
+        if self.start.next == None:
+            return self.delete_start()
+        while self.start.next is not None:
+            self.start.next=None
     def print_list(self):
         temp=self.start
         while temp is not None:
@@ -35,10 +66,15 @@ class SLL:
             temp=temp.next
 
 myList = SLL()
-myList.insert_at_start(5)
+# myList.insert_after(5,15)
+myList.insert_at_last(20)
 myList.insert_at_start(10)
-myList.insert_at_last(15)
+myList.insert_at_start(5)
+myList.delete_start()
+myList.delete_last()
+myList.delete_last()
+myList.delete_last()
 myList.print_list()
 
-result = myList.search(15)
-print('\nThis value have in list: ',result)
+
+# print("\nthis value is exist in list") if myList.search(15)  else print("Not exist")
