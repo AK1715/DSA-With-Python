5849c0f66515d22ba50704272c940d5570c2637b
AK1715 <ns031077@gmail.com>
2026-02-15 15:33:21 +0530

    instert on start

--- practice-circular.py
+++ practice-circular.py
@@ -4,26 +4,29 @@ class Node:
         self.next=next
 
 class circular:
-    def __init__(self,start=None):
-        self.start=start
+    def __init__(self,last=None):
+        self.last=last
     def insert_at_start(self,data):
-            n=Node(data,self.start)
-            self.start=n
-            temp=self.start
-            while temp.next is not None:
-                temp=temp.next
-                if temp.next is None:
-                    temp.next=self.start
-
+        n=Node(data)
+        if self.last is None:
+            n.next=n
+            self.last=n
+        else:
+            n.next=self.last.next
+            self.last.next=n
     def print_all(self):
-        temp = self.start
+        temp = self.last.next
         while temp is not None:
             print(temp.item, end=' ')
+            if temp == self.last:
+                break
             temp=temp.next
+        
 
 
 myList = circular()
 myList.insert_at_start(30)
 myList.insert_at_start(20)
 myList.insert_at_start(10)
+myList.insert_at_start(5)
 myList.print_all()
