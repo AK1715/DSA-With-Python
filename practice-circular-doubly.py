fef9887c55380a243e57e7948672ef9362a00af5
AK1715 <ns031077@gmail.com>
2026-02-19 18:35:38 +0530

    nai ho paya bhai

--- practice-circular-doubly.py
+++ practice-circular-doubly.py
@@ -3,4 +3,33 @@ class Node:
         self.prev=prev
         self.item=item
         self.next=next
-# class circularDoubly:
+class circularDoubly:
+    def __init__(self,start=None):
+        self.start=start
+    def insert_at_start(self,data):
+        n=Node(None,data,None)
+        if self.start is not None:
+            n.prev=self.start.prev
+            n.next=self.start
+            self.start.prev.next=n
+            self.start.prev=n
+            self.start=n
+        else:
+            self.start=n
+    def print_all(self):
+        temp=self.start
+        while temp.next != self.start:
+            print(temp.item, end=' ')
+            if temp == self.start.next:
+                break
+            temp=temp.next
+        
+            
+
+myList = circularDoubly()
+myList.insert_at_start(50)
+myList.insert_at_start(40)
+myList.insert_at_start(30)
+myList.insert_at_start(20)
+myList.insert_at_start(10)
+myList.print_all()
