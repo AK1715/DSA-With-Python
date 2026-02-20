2274d34382b2da840345d8cdbe29dd0f2d68ca21
AK1715 <ns031077@gmail.com>
2026-02-20 16:14:49 +0530

    insert after

--- practice-circular-doubly.py
+++ practice-circular-doubly.py
@@ -30,23 +30,43 @@ class circularDoubly:
             n.prev=n
             n.next=n
             self.start=n
+    def insert_after(self,tempValue,data):
+        n=Node(data)
+        if tempValue is not None:
+            if self.start is None:
+                return print("bhai tera list empty hai")
+            else:
+                temp=self.start
+                while temp.next != self.start:
+                    if temp.item == tempValue:
+                        n.next=temp.next
+                        n.prev=temp.next.prev
+                        temp.next.prev=n
+                        temp.next=n
+                    else:
+                        if temp.next == self.start:
+                            n.next=self.start
+                            n.prev=self.start.prev
+                            self.start.prev.next=n
+                        else:
+                            print("bhai tu jo value diya vo list me nai hai")
+                    temp=temp.next
     def print_all(self):
-        temp=self.start.next
-        print(self.start.item, end=' ')
-        while temp != self.start:
-            print(temp.item, end=' ')
-            temp=temp.next
+        if self.start is not None:
+            temp=self.start.next
+            print(self.start.item, end=' ')
+            while temp != self.start:
+                print(temp.item, end=' ')
+                temp=temp.next
         
             
 
 myList = circularDoubly()
-myList.insert_at_start(50)
-myList.insert_at_start(40)
-myList.insert_at_start(30)
 myList.insert_at_start(20)
-myList.insert_at_start(10)
-myList.insert_at_last(60)
-myList.insert_at_last(70)
-myList.insert_at_last(80)
-myList.insert_at_last(90)
+# myList.insert_at_start(10)
+myList.insert_at_last(30)
+# myList.insert_at_last(40)
+# myList.insert_after(10,15)
+myList.insert_after(10,15)
+
 myList.print_all()
